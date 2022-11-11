e2cb1b72707017d0261ffcb48c8dc808ab1a78db
rusj88 <49836247+rusj88@users.noreply.github.com>
2022-11-11 18:48:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useCallback } from "react";
+import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState([]);
+  const [background, setBackground] = useState("none");
+  const [result, setResult] = useState("");
+
+  const init = useCallback(() => {
+    setColors([getHexColor(), getHexColor(), getHexColor()]);
+  }, []);
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
+  useEffect(() => {
+    setBackground(colors[Math.floor(Math.random() * 3)]);
+  }, [colors]);
+
+  const getHexColor = () => {
+    const getRandomHex = () => Math.floor(Math.random() * 255).toString(16);
+    return (
+      "#" +
+      getRandomHex() +
+      getRandomHex() +
+      getRandomHex()
+    ).toUpperCase();
+  };
+
+  const handleClick = (color) => {
+    if (color === background) {
+      setResult("Correct");
+      init();
+    } else {
+      setResult("Incorrect");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="box" style={{ backgroundColor: background }}></div>
+        <div className="buttons">
+          {colors.map((ele, id) => {
+            return (
+              <button key={id} onClick={() => handleClick(ele)}>
+                {ele}
+              </button>
+            );
+          })}
+        </div>
+        {result && <div>{result}</div>}
+      </div>
     </div>
   );
 }
